47b4f42847304bad5c2ceeeb65f302a8e9d9ff99
MohmmadNada <mohammad.nada_97@yahoo.com>
2021-07-13 20:12:06 +0300

    finish today lab

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,30 @@
+from django.http import response
 from django.test import TestCase
-
+# from django.conf.urls import url
+from django.urls import reverse
+from .models import Snack
 # Create your tests here.
+class SnackTest(TestCase):
+    def test_list_page(self):
+        url=reverse('home')# return url link 
+        actual= self.client.get(url).status_code
+        excepted=200
+        self.assertEqual(actual,excepted)
+    def test_home_template(self):
+        url=reverse('home')
+        actual= self.client.get(url)
+        
+        excepted='snack_list.html'
+        self.assertEqual(actual.templates[0].name,excepted)
+    def setUp(self):
+        # self.snack
+        self.snack = Snack.objects.create(
+            title = 'testing test title',
+            purchaser='testing test purchaser',
+            description='testing test  description',
+        )
+    # def test_snack_list(self):
+    #     response=self.client.get('home')
+    #     print('=====>   ',response.items())
+    def test_movie_create(self):
+        response=self.client.post(reverse('create'),{})
